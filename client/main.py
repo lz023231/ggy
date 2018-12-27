9c25e37d6a4f7cb9c2a8ff9c4134ff810e06145d
lz023231 <15227001093@163.com>
2018-12-27 19:06:10 +0800

    写到加用户

--- client/main.py
+++ client/main.py
@@ -13,16 +13,54 @@ class ClientTest(unittest.TestCase):
         time.sleep(3)
         Login().login(self.driver, username, password)
         time.sleep(2)
+    def pubApp(self):
+        self.driver.find_element_by_xpath("//a[contains(text(),'发布应用')]").click()
+    def addUser(self):
+        self.driver.find_element_by_xpath("//a[contains(text(),'加用户')]").click()
+    def clickUser(self):
+        self.driver.find_element_by_xpath('//a[contains(text(),"客户")]').click()
+    def selectHost(self):
+        self.driver.find_element_by_xpath("//table/*/tr/td/div/input[@value='12879']").click()
     def test_1_client(self):
         #path1='//a[contains(text(),"客户")]'
-        self.driver.find_element_by_xpath('//a[contains(text(),"客户")]').click()
-        time.sleep(2)
+        #self.driver.find_element_by_xpath('//a[contains(text(),"客户")]').click()
+        self.clickUser()
+        time.sleep(1)
     def test_2_showClient(self):
         #path = '//a[contains(text(),"显示更多")]'
-        self.driver.find_element_by_xpath('//a[contains(text(),"显示更多")]').click()
-        time.sleep(2)
+        self.driver.find_element_by_xpath("//a[contains(text(),'添加客户')]/following-sibling::a[contains(text(),'显示更多')]").click()
+        time.sleep(1)
+        #发布应用
+        #self.driver.find_element_by_xpath("//table/*/tr/td/div/input[@value='12879']").click()
+        self.selectHost()
+        time.sleep(1)
+        self.pubApp()
+        #self.driver.find_element_by_xpath("//a[contains(text(),'发布应用')]").click()
+        self.driver.find_element_by_xpath("//label[contains(text(),'cmd')]").click()
+        time.sleep(1)
+        self.driver.find_element_by_xpath("//input[@value='取消']").click()
+        time.sleep(1)
+        self.pubApp()
+        self.driver.find_element_by_xpath("//label[contains(text(),'cmd')]").click()
+        #self.driver.find_element_by_xpath("//a")
+        self.driver.find_element_by_xpath("//input[@value='确定']").click()
+        time.sleep(1)
+        self.driver.find_element_by_xpath("//table/*/tr/td/div/input[@value='12879']").click()
+        time.sleep(1)
+        self.pubApp()
+        time.sleep(1)
+        #self.driver.find_element_by_xpath("//input[@value='取消']").cleck()
+
+        #加用户
+    def test_3_addUserS(self):
+        self.clickUser()
+        self.selectHost()
+        self.addUser()
+        time.sleep(1)
+
 
     def tearDown(self):
+        time.sleep(1)
         self.driver.quit()
 
 if __name__ == '__main__':
